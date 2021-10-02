284385085bd945f6d94275af54f78ebe682cdff2
Jessica Alejandra Nuñez Trevizo <42632200+16550545@users.noreply.github.com>
2021-10-02 15:42:47 -0600

    commit primer actualización
    El personaje se mueve de izquierda a derecha

---
+++ SpaceApps-Juego/Personaje.gd
@@ -0,0 +1,31 @@
+extends Area2D
+export (int) var Velocidad
+#ayuda a posicionar nuestro personaje
+var Movimiento = Vector2()
+var Limite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Limite = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	Movimiento = Vector2() #reinicia el valor
+	#para ir a la derecha
+	if Input.is_action_pressed("ui_right"):
+		Movimiento.x += 1
+	#para ir a la izquierda
+	if Input.is_action_pressed("ui_left"):
+		Movimiento.x -=1
+	#verifica si se esta moviendo para normalizar la velocidad
+	if Movimiento.length() > 0:
+		Movimiento = Movimiento.normalized() * Velocidad
+	#valor de posicion, actualiza los movimientos 
+	position += Movimiento * delta
+	position.x = clamp(position.x, 0, Limite.x)
+	
+	if Movimiento.x != 0:
+		$AnimatedSprite.animation = "Caminando"
+		$AnimatedSprite.flip_h = Movimiento.x > 0
+	else:
+		$AnimatedSprite.animation = "Frente"
